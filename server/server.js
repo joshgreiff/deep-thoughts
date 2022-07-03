ef45bf4abe2845b87cf551519fd633bf186448df
Josh Greiff <josh@greiff.org>
2022-07-03 03:51:31 -0400

    finished 21.3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path')
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require('./utils/auth')
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas')
@@ -10,7 +13,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: (authMiddleware)
 }) 
 
 const app = express();
@@ -24,6 +28,14 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our apollo server with the express application as middleware
   server.applyMiddleware({ app })
 
+  // serve up static assets 
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  })
 
   db.once('open', () => {
     app.listen(PORT, () => {
