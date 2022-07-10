eb392c358aa266be643fe269a018ea091c951037
Josh Greiff <josh@greiff.org>
2022-07-10 01:11:13 -0400

    finished 21.6.6

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,9 +3,11 @@ import { Navigate, useParams } from 'react-router-dom'
 
 import ThoughtList from '../components/ThoughtList'
 import FriendList from '../components/FriendList'
+import ThoughtForm from '../components/ThoughtForm'
 
-import { useQuery } from '@apollo/client'
+import { useQuery, useMutation } from '@apollo/client'
 import { QUERY_USER, QUERY_ME } from '../utils/queries'
+import { ADD_FRIEND } from '../utils/mutations'
 import Auth from '../utils/auth'
 
 const Profile = () => {
@@ -14,6 +16,8 @@ const Profile = () => {
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   })
+
+  const [addFriend] = useMutation(ADD_FRIEND)
 
   const user = data?.me || data?.user || {}
 
@@ -33,12 +37,27 @@ const Profile = () => {
     )
   }
 
+  const handleClick = async () => {
+    try{
+      await addFriend({
+        variables: { id: user._id }
+      })
+    }catch(e){
+      console.error(e)
+    } 
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+      {userParam && (
+        <button className='btn ml-auto' onClick={handleClick}>
+          Add Friend
+        </button>
+      )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -54,6 +73,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className='mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,9 @@ import { useQuery } from '@apollo/client'
 import { QUERYTHOUGHT } from '../utils/queries'
 
 import ReactionList from '../components/ReactionList'
+import ReactionForm from '../components/ReactionForm';
+
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams()
@@ -33,6 +36,8 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+
+      {Auth.loggedIn() && <ReactionForm thought={thought._id} />}
     </div>
   );
 };
